0b4e2bdf7a9fc288e0470b7d6ee554a2519f87eb
Timur <timurtein@gmail.com>
2024-09-17 21:24:05 +0300

    labWork 3

--- projects/triangle/main.go
+++ projects/triangle/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func getGap(a float64, b float64) float64 {
+	var c float64 = math.Sqrt(a*a + b*b)
+	return c
+}
 
 func main() {
-	fmt.Println("Hello, world!!!")
+	var a, b float64
+	fmt.Scanf("%f %f", &a, &b)
+	fmt.Println(getGap(a, b))
 }
