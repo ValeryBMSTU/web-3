5b4be9c4d824095512c2e3d34daac2b16cb0815a
lark-devp <botrazumniy@gmail.com>
2024-09-29 13:02:51 +0300

    starsStarsStars

--- projects/star/main.go
+++ projects/star/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "strings"
+)
 
 func main() {
-	fmt.Println("Hello, world!!!")
+    var str string
+    fmt.Scan(&str)
+    
+    res:= strings.Join(strings.Split(str,""),"*")
+    
+    fmt.Println(res)
 }
+
+
+
+
+
