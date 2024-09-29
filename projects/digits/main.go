2e75aaf3865239ad89c4ddb684d6b071f1cafff0
lark-devp <botrazumniy@gmail.com>
2024-09-29 13:22:06 +0300

    arabicDigits

--- projects/digits/main.go
+++ projects/digits/main.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("Hello, world!!!")
+func getMaxDigit(str string) int{
+    maxD := 0
+    for _, elem := range str{
+        d := int(elem - '0')
+        if d > maxD{
+            maxD = d 
+        }
+    }
+    return maxD
 }
+
+func main() {
+    var s string
+    _, _ = fmt.Scan(&s)
+    fmt.Println(getMaxDigit(s))
+}
+
+
+
+
+
