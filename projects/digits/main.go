5afafe49f04d3bf26588bc17435f4ff043111924
EnderYeekkay <nikita.kolobrodov.02@mail.ru>
2024-10-30 15:45:18 +0300

    digits

--- projects/digits/main.go
+++ projects/digits/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, world!!!")
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		var input string = scanner.Text()
+		var max uint = 0
+		for _, val := range input {
+			a := uint(val - '0')
+			if a > max {
+				max = a
+			}
+		}
+		fmt.Println(max)
+	}
 }
