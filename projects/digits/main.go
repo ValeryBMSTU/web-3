4fe0043d90d74800f61f60d10843736ee0495ac9
Diamkey <dmigred@mail.ru>
2024-11-28 13:45:29 +0400

    pdf

--- projects/digits/main.go
+++ projects/digits/main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world!!!")
+	var st string
+	var m int = 0
+	fmt.Println("Введите строку из цифр:")
+	fmt.Scan(&st)
+	for i := 0; i < len(st); i++ {
+		tmp := int(st[i] - '0')
+		if tmp > m {
+			m = tmp
+		}
+	}
+	fmt.Println("Наибольшая цифра:", m)
 }
--- projects/triangle/main.go
+++ projects/triangle/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Println("Hello, world!!!")
+	fmt.Println("Введите катеты:")
+	var a, b int
+	fmt.Scan(&a, &b)
+	fmt.Println(math.Sqrt(float64(a*a + b*b)))
 }
